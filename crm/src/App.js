188c334329f94257183d8d097218363bf51bda30
colinah <halecolin1@gmail.com>
2020-04-13 15:32:56 -0600

    Adding general layout

--- crm/src/App.js
+++ crm/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import Banner from './containers/Banner/Banner';
+import classes from './App.module.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.BannerContainer}>
+        <Banner />
+      </div>
+      <div className={classes.PageContainer}>
+        <div className={classes.ProfileContainer}>
+          <div>Profile info</div>
+        </div>
+        <div className={classes.ContentContainer}>
+          <div>Page Content</div>
+        </div>
+        <div className={classes.SideBarContainer}>
+          <div>Side Bar</div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
---
+++ crm/src/containers/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Banner.module.scss';
+
+
+const nav = () => {
+return (
+    <div className={classes.BannerContainer}>
+            <div>
+                Crux Software Logo
+            </div>
+    </div>
+)
+}
+
+export default nav;
